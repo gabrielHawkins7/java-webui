2e80cc2c3f913778fd072bfcd7fef58f6329eec0
GabrielHawkins7 <hawkinsglh@outlook.com>
2025-08-20 15:14:16 -0400

    Testing

---
+++ src/test/WebuiTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WebuiTest {
+    @Test
+    @DisplayName("A simple assertion to verify the test runner is working.")
+    void simpleAssertion() {
+        assertTrue(true);
+    }
+}
